1767255f7645bc3dc47edd235f3b36baa886c086
Irving Eslava <iaeg93@gmail.com>
2019-04-22 14:46:23 -0500

    Cambia rutas de axios de localhost a rutas relativas.

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   // our first get method that uses our backend api to
   // fetch data from our data base
   getDataFromDb = () => {
-    fetch("http://localhost:3001/api/getData")
+    fetch("/api/getData")
         .then(data => data.json())
         .then(res => this.setState({ data: res.data }));
   };
@@ -54,7 +54,7 @@ class App extends Component {
       ++idToBeAdded;
     }
 
-    axios.post("http://localhost:3001/api/putData", {
+    axios.post("/api/putData", {
       id: idToBeAdded,
       message: message
     });
@@ -71,7 +71,7 @@ class App extends Component {
       }
     });
 
-    axios.delete("http://localhost:3001/api/deleteData", {
+    axios.delete("/api/deleteData", {
       data: {
         id: objIdToDelete
       }
@@ -89,7 +89,7 @@ class App extends Component {
       }
     });
 
-    axios.post("http://localhost:3001/api/updateData", {
+    axios.post("/api/updateData", {
       id: objIdToUpdate,
       update: { message: updateToApply }
     });
